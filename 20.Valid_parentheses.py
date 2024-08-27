a96b78409502274aac61b920a0f153d458eaae3c
Artem <artni96@yandex.ru>
2024-08-27 11:43:57 +0300

    task 20 done

--- 20.Valid_parentheses.py
+++ 20.Valid_parentheses.py
@@ -4,48 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-        round_brackets = 0
-        figure_brackets = 0
-        square_brackets = 0
         brackets_types = {
             '(': ')',
             '[': ']',
             '{': '}'
         }
-        brackets_value = {
-            '(': round_brackets,
-            '[': square_brackets,
-            '{': figure_brackets
-        }
         count = 0
         unclosed_pair = []
-        if len(s) >= 2 and s[0] in brackets_types.keys():
+        if len(s) >= 2 and len(s) % 2 == 0:
             while count < len(s):
-                try:
-                    current_obj = s[count]
-                    brackets_value[current_obj] += 1
-                    if s[count + 1] == brackets_types[current_obj]:
-                        count += 2
-                        brackets_value[current_obj] -= 1
+                current_obj = s[count]
+                if current_obj in brackets_types:
+                    if count + 1 <= len(s) - 1:
+                        if s[count + 1] == brackets_types[current_obj]:
+                            count += 2
+                        else:
+                            count += 1
+                            unclosed_pair.append(current_obj)
                     else:
-                        count += 1
-                        unclosed_pair.append(current_obj)
-                except Exception:
-                    if brackets_types[unclosed_pair[-1]] == current_obj:
-                        brackets_value[unclosed_pair[-1]] -= 1
+                        if len(unclosed_pair) > 0:
+                            return False
+                elif len(unclosed_pair) >= 1:
+                    if current_obj == brackets_types[unclosed_pair[-1]]:
                         del unclosed_pair[-1]
                         count += 1
-                    count += 1
-
-            for elem in brackets_value.values():
-                if elem > 0:
+                    elif current_obj != brackets_types[unclosed_pair[-1]]:
+                        return False
+                else:
                     return False
+            if len(unclosed_pair) > 0 and count == len(s):
+                return False
             return True
         return False
-
-
-
 
 
 test_data_1 = '()'
@@ -54,9 +44,15 @@ test_data_3 = '(]'
 test_data_4 = '([])'
 test_data_5 = '['
 test_data_6 = '){'
+test_data_7 = '(){}}{'
+test_data_8 = '(('
+test_data_9 = '[[[[[[[[[[[[[[[[[[['
 print(Solution().isValid(test_data_1))
 print(Solution().isValid(test_data_2))
 print(Solution().isValid(test_data_3))
 print(Solution().isValid(test_data_4))
 print(Solution().isValid(test_data_5))
 print(Solution().isValid(test_data_6))
+print(Solution().isValid(test_data_7))
+print(Solution().isValid(test_data_8))
+print(Solution().isValid(test_data_9))
