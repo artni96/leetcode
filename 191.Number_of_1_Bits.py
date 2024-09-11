a26ff02419136370e9723c18ec8909b3f41a74c0
Artem <artni96@yandex.ru>
2024-09-11 23:24:25 +0300

    problem 191 solved

---
+++ 191.Number_of_1_Bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return bin(n)[2:].count('1')
+
+
+print(Solution().hammingWeight(1010))
